1a38a16c646c29a7facf7b49502671013b5fd2b3
Ravishr1996 <rrp.star7@gmail.com>
2016-09-25 21:07:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article-one', function (req,res){
+    res.send('I am requesting article one to be published'); 
+});
+
+app.get('/article-two', function (req,res){
+    res.send('I am requesting article two to be published'); 
+});
+
+app.get('/article-three', function (req,res){
+    res.send('I am requesting article three to be published'); 
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
