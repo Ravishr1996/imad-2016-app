108d2b185a2eb8075a502511a95aa1442290d564
Ravishr1996 <rrp.star7@gmail.com>
2016-10-12 21:32:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,70 +6,7 @@ var app = express();
 app.use(morgan('combined'));
 
 
-var articleOne ={
-    title: 'Article one By RRP',
-    heading: ' Article One By RRP ',
-    date: 'Sept 25th 2016',
-    content: ` <p>
-                    This Is The Content of My first Article Made a begginer programmer ravi roshan pandey.. Pls wish me Luck So i can use up my skills and be the best programmer in the world.
-                </p>
-           
-                     <p>
-                    This Is The Content of My first Article Made a begginer programmer ravi roshan pandey.. Pls wish me Luck So i can use up my skills and be the best programmer in the world.
-                    </p>
-           
-                        <p>
-                        This Is The Content of My first Article Made a begginer programmer ravi roshan pandey.. Pls wish me Luck So i can use up my skills and be the best programmer in the world.
-                         </p>`
-    
-    
-    
-    
-};
 
-function createTemplate (data)  {
-    var title = data.title;
-    var heading = data.heading;
-    var date = data.date;
-    var content = data.content; 
-    
-    var htmlTemplate = `<html>
-    <head>
-       <title>
-           ${tilte}
-       </title>
-       <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link href="/ui/style.css" rel="stylesheet" />
-   </head> 
-   <body>
-       <div class="container">
-         
-        <div>
-          <a herf="/">Home</a> 
-    
-        </div>
-        <hr/>
-        <h3>
-            ${haeding}
-        </h3>
-        <div>
-           ${date}
-        </div>
-        <div>
-           ${content}
-        </div>
-        </div>
-        <div>
-            <script>
-                 alert (" This is the javascript of the Article one ");
-                 alert ( " This is the 2nd line of the java script of the article one ");
-            </script>
-        </div>
-   </body>
- </html>
- `; 
- return htmlTemplate;
-}
 app.get('/', function (req, res) {
  res.send(createTemplate(articleOne));
 });
