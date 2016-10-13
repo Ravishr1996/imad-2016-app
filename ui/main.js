e4974e0f2bfc9a13f1a575276f2dc1dcd0ccb587
Ravishr1996 <rrp.star7@gmail.com>
2016-10-13 20:16:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,10 @@
 console.log('Loaded!');
 var element = document.getElementById('main-text');
 element.innerHTML = ' ravi roshan pandey';
+
+var img = document.getElementById('madi');
+img.onclick = function() {
+    img.style.marginleft = '100px';
+    
+    
+};
