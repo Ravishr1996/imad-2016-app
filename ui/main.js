4974937e3f8625d9ec617d5bfc9108b56b0be8e2
Ravishr1996 <rrp.star7@gmail.com>
2016-10-14 15:27:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,13 +1,23 @@
 
 
-
+// Counter code
 var button = document.getElementById('counter');
-var counter = 0;
+
 button.onclick = function() {
-    
-    
-    counter=counter + 1;
-    var span= document.getElementById('count');
-    span.innerHTML = counter.toString(); 
-    
+   // create a request object
+    var request = new XMLHttpRequest();
+//create the response and store it in a variable
+    request.onreadystatechange = function () {
+        if (request.readyState === XMLHttpRequest.DONE){
+         if (request.status === 200 ){
+            var counter = request.responseText;
+            var span = document.getElememtById('count');
+            span.innerHTML = counter.toString();
+         }
+        }
+        //not done yet
+    };
+    //make the request
+    request.open('GET','http://http://ravishr1996.imad.hasura-app.io/', true);
+    request.send(null);
 };
