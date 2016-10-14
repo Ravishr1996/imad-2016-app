3f0e95852ab745626d7a7069ed4165d2d8d1efc0
Ravishr1996 <rrp.star7@gmail.com>
2016-10-14 14:22:23 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -11,3 +11,14 @@ function moveLeft() {
 img.onclick = function() {
     var interval = setInterval(moveLeft,20);
 };
+
+var button = document.getElementById('counter');
+var counter = 0;
+button.onclick = function() {
+    
+    
+    counter=counter + 1;
+    var span= document.getElementById('count');
+    span.innerHTML = counter.toString(); 
+    
+};
