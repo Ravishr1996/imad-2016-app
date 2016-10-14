c45f193c8db35cfe763951407a7a4c399c17384f
Ravishr1996 <rrp.star7@gmail.com>
2016-10-14 17:30:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,23 +1,19 @@
 
 var button = document.getElementById('counter');
 button.onclick = function() {
-    counter=counter + 1;
-    
-    
-
-
-
-var request = new XMLHttpRequest();
+   
+   var request = new XMLHttpRequest();
+   
    request.onreadystatechange = function () {
         if (request.readyState === XMLHttpRequest.DONE){
-         if (request.status === 200){
-            var counter = request.responseText;
-            var span = document.getElementById('count');
-            span.innerHTML = counter.toString();
+             if (request.status === 200){
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
          }
         }
     };
 
-request.open('GET','http://http://ravishr1996.imad.hasura-app.io/counter', true);
+    request.open('GET','http://http://ravishr1996.imad.hasura-app.io/counter', true);
     request.send(null);
 };
