0516535e4b20c0532b50371ba3d627d7a64f1507
Ravishr1996 <rrp.star7@gmail.com>
2016-10-19 22:00:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -26,13 +26,7 @@ app.get('/submit-name',function (req,res) {
     
 });
 
-var names=[];
-app.get('/article-one',function (req,res) {
-    var name= req.query.name;
-    names.push(name);
-    res.send(JSON.stringify(names));
-    
-});
+
 
 
  
