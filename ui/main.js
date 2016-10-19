072d39497b27c1376321a515867d8f05ef5fac37
Ravishr1996 <rrp.star7@gmail.com>
2016-10-19 23:17:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -46,7 +46,7 @@ submit.onclick=function() {
     };
     var nameInput=document.getElementById('name');
     var name = nameInput.value; 
-    request.open('GET','http://ravishr1996.imad.hasura-app.io/submit-name?name= '+name, true);
+    request.open('GET','http://ravishr1996.imad.hasura-app.io/article-name?name= '+name, true);
     request.send(null);
     
    //capture a list of names and render it as a list
