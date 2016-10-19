99b0570669d5d611918e6a498438a4408679fc5b
Ravishr1996 <rrp.star7@gmail.com>
2016-10-19 18:33:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -24,7 +24,26 @@ var nameInput=document.getElementById('name');
 var name = nameInput.value;
 var submit=document.getElementById('submit_btn');
 submit.onclick=function() {
+    
     //make a request to the server and send the name 
+    var button = document.getElementById('counter');
+
+button.onclick = function() {
+   
+   var request = new XMLHttpRequest();
+   
+   request.onreadystatechange = function () {
+        if (request.readyState === XMLHttpRequest.DONE){
+             if (request.status === 200){
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+             }
+        }
+    };
+
+    request.open('GET','http://ravishr1996.imad.hasura-app.io/counter', true);
+    request.send(null);
     
    //capture a list of names and render it as a list
   
