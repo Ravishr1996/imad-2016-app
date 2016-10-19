bf1c80d0c8594bb9d9b4e0c83e9fe5b009ed108e
Ravishr1996 <rrp.star7@gmail.com>
2016-10-19 22:03:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -51,3 +51,36 @@ submit.onclick=function() {
   
  
 };
+
+//submit name
+var nameInput=document.getElementById('name');
+var name = nameInput.value;
+var submit=document.getElementById('submit_btn');
+submit.onclick=function() {
+   var request = new XMLHttpRequest();
+   
+   request.onreadystatechange = function () {
+        if (request.readyState === XMLHttpRequest.DONE){
+             if (request.status === 200){
+                  var names= request.responseText;
+                  names = JSON.parse(names);
+                  var list=''; 
+                  for(var i=0; i<names.length; i++){
+                      list += '<li>' + names[i] + '</li>';
+                      
+                  }
+                    var ul = document.getElementById('namelist');
+                    ul.innerHTML = list; 
+                               
+             }
+        } 
+    };
+    var nameInput=document.getElementById('name');
+    var name = nameInput.value; 
+    request.open('GET','http://ravishr1996.imad.hasura-app.io/article-one'+name, true);
+    request.send(null);
+    
+   //capture a list of names and render it as a list
+  
+ 
+};
