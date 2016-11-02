b771d943893046b8564844bba0b64255ff25a06a
Ravishr1996 <rrp.star7@gmail.com>
2016-11-02 19:34:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.get('/submit-name',function (req,res) {
 });
 
 var comments=[];
-app.get('/submit-comment',function (req,res) {
+app.get('/submitt-comment',function (req,res) {
     var comment= req.query.comment;
      comments.push(comment);
     res.send(JSON.stringify(comments));
